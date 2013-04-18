df69a63c24e8a97d2967ca5806fae9f0b672994a
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2013-04-18 13:27:57 +0200

    Small adjustments to table stuff

--- scripts/table.js
+++ scripts/table.js
@@ -7,7 +7,7 @@ var H5P = H5P || {};
  * @param {string} contentPath The path to our content folder.
  */
 H5P.Table = function (params, contentPath) {
-  this.text = params.text === undefined ? '<table><tr><th>New</th><td></td></tr><tr><td></td><td>table</td></tr></table>' : params.text;
+  this.text = params.text === undefined ? '<table border="1" cellpadding="1" cellspacing="1" style="width: 100%;"><thead><tr><th scope="col">1-1</th><th scope="col">1-2</th></tr></thead><tbody><tr><td>2-1</td><td>2-2</td></tr><tr><td>3-1</td><td>3-2</td></tr></tbody></table>' : params.text;
 };
 
 /**
