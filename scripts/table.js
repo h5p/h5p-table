7247c5709299a9273d00bc60841b1873f6e63e10
Frode Petterson <frode.petterson@amendor.com>
2013-06-06 12:35:43 +0200

    Changed table default value text and added translations. Also made custom styling the mother librarys job.

--- scripts/table.js
+++ scripts/table.js
@@ -2,17 +2,17 @@ var H5P = H5P || {};
 
 /**
  * Constructor.
- * 
+ *
  * @param {object} params Options for this library.
  * @param {string} contentPath The path to our content folder.
  */
 H5P.Table = function (params, contentPath) {
-  this.text = params.text === undefined ? '<table border="1" cellpadding="1" cellspacing="1" style="width: 100%;"><thead><tr><th scope="col">1-1</th><th scope="col">1-2</th></tr></thead><tbody><tr><td>2-1</td><td>2-2</td></tr><tr><td>3-1</td><td>3-2</td></tr></tbody></table>' : params.text;
+  this.text = params.text === undefined ? '<table class="h5p-table"><thead><tr><th scope="col">Heading Column 1</th><th scope="col">Heading Column 2</th></tr></thead><tbody><tr><td>Row 1 Col 1</td><td>Row 1 Col 2</td></tr><tr><td>Row 2 Col 1</td><td>Row 2 Col 2</td></tr></tbody></table>' : params.text;
 };
 
 /**
  * Wipe out the content of the wrapper and put our HTML in it.
- * 
+ *
  * @param {jQuery} $wrapper
  */
 H5P.Table.prototype.attach = function ($wrapper) {
