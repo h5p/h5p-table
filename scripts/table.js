49826944280e38875c2469a186c4eddcd8c5ea0c
Pål Jørgensen <pal.jorgensen@amendor.com>
2013-07-09 18:49:40 +0200

    OPPG-416 - fixes after review

--- scripts/table.js
+++ scripts/table.js
@@ -4,9 +4,9 @@ var H5P = H5P || {};
  * Constructor.
  *
  * @param {object} params Options for this library.
- * @param {string} contentPath The path to our content folder.
+ * @param {int} id Content identifier
  */
-H5P.Table = function (params, contentPath) {
+H5P.Table = function (params, id) {
   this.text = params.text === undefined ? '<table class="h5p-table"><thead><tr><th scope="col">Heading Column 1</th><th scope="col">Heading Column 2</th></tr></thead><tbody><tr><td>Row 1 Col 1</td><td>Row 1 Col 2</td></tr><tr><td>Row 2 Col 1</td><td>Row 2 Col 2</td></tr></tbody></table>' : params.text;
 };
 
