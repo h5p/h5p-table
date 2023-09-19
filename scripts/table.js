3b635a5e8ebbbda71537eebcf7167e7fc0765baf
Vilde Stabell <vilde.voss.stabell@gmail.com>
2023-09-19 09:50:36 +0200

    JI-5366 Add headers to default table

--- scripts/table.js
+++ scripts/table.js
@@ -8,19 +8,23 @@ var H5P = H5P || {};
  */
 H5P.Table = function (params, id) {
   const defaultTable = '<figure class="table">' +
-                          '<table>' +
-                            '<tr>' +
-                              '<td><strong>Heading Column 1</strong></td>' +
-                              '<td><strong>Heading Column 2</strong></td>'+
-                            '</tr>' +
-                            '<tr>' +
-                              '<td>Row 1 Col 1</td>' +
-                              '<td>Row 1 Col 2</td>' +
-                            '</tr>' +
-                            '<tr>' +
-                              '<td>Row 2 Col 1</td>' +
-                              '<td>Row 2 Col 2</td>' +
-                            '</tr>' +
+                          '<table class="h5p-table">' +
+                            '<thead>' +
+                              '<tr>' +
+                                '<th>Heading Column 1</th>' +
+                                '<th>Heading Column 2</th>'+
+                              '</tr>' +
+                            '</thead>' +
+                            '<tbody>' +
+                              '<tr>' +
+                                '<td>Row 1 Col 1</td>' +
+                                '<td>Row 1 Col 2</td>' +
+                              '</tr>' +
+                              '<tr>' +
+                                '<td>Row 2 Col 1</td>' +
+                                '<td>Row 2 Col 2</td>' +
+                              '</tr>' +
+                            '</tbody>' +
                           '</table>' +
                         '</figure>'
   this.text = params.text === undefined ? defaultTable : params.text;
