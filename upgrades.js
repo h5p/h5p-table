e7cc9e8ace55ed3977021de52d167884f6e56b66
Vilde Stabell <vilde.voss.stabell@gmail.com>
2024-01-19 16:02:54 +0100

    JI-5423 Upgrade cke4 border styles

---
+++ upgrades.js
@@ -0,0 +1,62 @@
+/** @namespace H5PUpgrades */
+var H5PUpgrades = H5PUpgrades || {};
+
+H5PUpgrades['H5P.Table'] = (function () {
+  return {
+    1: {
+      2: function (parameters, finished, extras) {
+        const tables = parameters.text.split('<table');
+        let newParams = tables[0];
+
+        for (let i = 1; i < tables.length; i++) {
+          const style = 'style="border-style:solid;';
+
+          if (tables[i].includes('border')) {
+            // Set border style on the table
+            if (tables.includes('style="')) {
+              tables[i] = tables[i].replace('style="', style);
+            }
+            else {
+              tables[i] = ' ' + style + '"' + tables[i];
+            }
+
+            // Set border style on header cells
+            let headers = tables[i].split(/<th(?!ead)/);
+            tables[i] = headers[0];
+
+            for (let j = 1; j < headers.length; j++) {
+              tables[i] += '<th';
+
+              if (headers[j].includes('style="')) {
+                tables[i] += headers[j].replace('style="', style);
+              }
+              else {
+                tables[i] += ' ' + style + '"' + headers[j];
+              }
+            }
+            
+            // Set border style on cells
+            let cells = tables[i].split('<td');
+            tables[i] = cells[0];
+
+            for (let j = 1; j < cells.length; j++) {
+              tables[i] += '<td';
+
+              if (cells[j].includes('style="')) {
+                tables[i] += cells[j].replace('style="', style);
+              }
+              else {
+                tables[i] += ' ' + style + '"' + cells[j];
+              }
+            }
+          }
+
+          newParams += '<table' + tables[i];
+        }
+
+        parameters.text = newParams;
+        finished(null, parameters, extras);
+      }
+    }
+  };
+})();
