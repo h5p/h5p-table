4f1a27daeded7d33a7310b48ba23ed5826a999ce
Vilde Stabell <vilde.voss.stabell@gmail.com>
2024-01-25 13:31:26 +0100

    JI-5423 Upgrade cellpadding regardless of border

--- upgrades.js
+++ upgrades.js
@@ -9,31 +9,41 @@ H5PUpgrades['H5P.Table'] = (function () {
         let newParams = tables[0];
 
         for (let i = 1; i < tables.length; i++) {
+          let tableChanges = false;
+          let cellChanges = false;
+          let style = 'style="';
+          let cellStyle = 'style="';
+
+          // Find and replace border width and style
           if (tables[i].includes('border')) {
-            // Find and replace border width
-            let needle = 'border="';
-            let needleStart = tables[i].indexOf(needle);
-            let needleEnd = tables[i].indexOf('"', needleStart + needle.length);
+            const needle = 'border="';
+            const needleStart = tables[i].indexOf(needle);
+            const needleEnd = tables[i].indexOf('"', needleStart + needle.length);
             const borderWidth = parseInt(tables[i].substring(needleStart + needle.length, needleEnd));
 
-            const style = 'style="border-style:solid;border-collapse:collapse;' + 'border-width:' + borderWidth + 'px;';
-            let cellStyle = 'style="border-style:solid;';
+            style += 'border-style:solid;border-collapse:collapse;' + 'border-width:' + borderWidth + 'px;';
+            cellStyle += tables[i].includes('h5p-table') ?
+              'border-style:solid;' :
+              'border-style:double;border-collapse:collapse;border-width:0.2em;';
 
-            if (!tables[i].includes('h5p-table')) {
-              cellStyle = 'style="border-style:double;border-collapse:collapse;border-width:0.2em;';
-            }
+            tableChanges = true;
+            cellChanges = true;
+          }
 
-            // Find and replace cell padding
-            if (tables[i].includes('cellpadding')) {
-              needle = 'cellpadding="';
-              needleStart = tables[i].indexOf(needle);
-              needleEnd = tables[i].indexOf('"', needleStart + needle.length);
-              const cellPadding = parseInt(tables[i].substring(needleStart + needle.length, needleEnd));
+          // Find and replace cell padding
+          if (tables[i].includes('cellpadding')) {
+            const needle = 'cellpadding="';
+            const needleStart = tables[i].indexOf(needle);
+            const needleEnd = tables[i].indexOf('"', needleStart + needle.length);
+            const cellPadding = parseInt(tables[i].substring(needleStart + needle.length, needleEnd));
 
-              cellStyle += 'padding:' + cellPadding + 'px;';
-              tables[i] = tables[i].replace(/cellpadding="[0-9]*"/, '');
-            }
+            cellStyle += 'padding:' + cellPadding + 'px;';
+            tables[i] = tables[i].replace(/cellpadding="[0-9]*"/, '');
 
+            cellChanges = true;
+          }
+
+          if (tableChanges) {
             // Set border style on the table
             if (tables.includes('style="')) {
               tables[i] = tables[i].replace('style="', style);
@@ -41,7 +51,9 @@ H5PUpgrades['H5P.Table'] = (function () {
             else {
               tables[i] = ' ' + style + '"' + tables[i];
             }
+          }
 
+          if (cellChanges) {
             // Set border style on header cells
             let headers = tables[i].split(/<th(?!ead)/);
             tables[i] = headers[0];
@@ -56,7 +68,7 @@ H5PUpgrades['H5P.Table'] = (function () {
                 tables[i] += ' ' + cellStyle + '"' + headers[j];
               }
             }
-            
+
             // Set border style on cells
             let cells = tables[i].split('<td');
             tables[i] = cells[0];
